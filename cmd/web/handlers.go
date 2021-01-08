e25ed48d553d9bb46b050ff0c7dd71293046f68d
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-01-08 17:44:11 -0500

    handle case where email is already taken when signing up

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -132,7 +132,30 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 		app.render(w, r, "signup.page.tmpl", &templateData{
 			Form: form,
 		})
+		return
 	}
+
+	// Try to create a new user record in the database. If the email already
+	// exists, add an error message to the form and re-display it.
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", "Email is already in use")
+			app.render(w, r, "signup.page.tmpl", &templateData{
+				Form: form,
+			})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Otherwise add a confirmation flash message to the session confirming
+	// that their signup worked and asking them to log in.
+	app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+
+	// And redirect the user to the login page.
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
