c25d28ac4a40636dd698f93eacb503a3f5fb4041
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-01-08 16:55:50 -0500

    create a User and UserModel struct with placeholder methods for accessing the users table

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,12 +5,33 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	// ErrNoRecord error when user tries to find a record that
+	// doesn't exist.
+	ErrNoRecord = errors.New("models: no matching record found")
+	// ErrInvalidCredentials error when a user tries to login with
+	// an incorrect email address or password.
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	// ErrDuplicateEmail error when a user tries to signup with an
+	// email address that's already in use.
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
 
+// Snippet type
 type Snippet struct {
 	ID      int
 	Title   string
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+// User type
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+	Active         bool
 }
